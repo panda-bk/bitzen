756404202bcd8cb0d89dc17bb3fa559dfc6b8122
pandabk <davidrichard.ms@gmail.com>
2019-04-16 08:31:17 -0300

    script

---
+++ script-windows.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo Iniciando o Docker
+docker-compose up -d
+
+echo 
+docker exec -it bitzen-app composer install
+
+echo Gerando key
+docker exec -it bitzen-app php artisan key:generate
+
+echo Executando Migrate/Seeds
+docker exec -it bitzen-app php artisan migrate --seed
